8b38aa4a8135e440bb7dc57eccbd97f3743ac909
Shufan Shahi <shufanshahi@gmail.com>
2025-05-31 05:36:48 +0600

    login done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,52 @@
 import { Stack } from "expo-router";
 import { Platform, StatusBar, View } from "react-native";
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import { Slot, useRouter, useSegments } from 'expo-router';
+import { useEffect } from 'react';
+import * as SecureStore from 'expo-secure-store';
+
+const CLERK_PUBLISHABLE_KEY = process.env.EEXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+
+const InitialLayout = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const inTabsGroup = segments[0] === '(tabs)';
+
+    console.log('User changed: ', isSignedIn);
+
+    if (isSignedIn && !inTabsGroup) {
+      router.replace('/profile');
+    } else if (!isSignedIn) {
+      router.replace('/login');
+    }
+  }, [isSignedIn]);
+
+  return <Slot />;
+};
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
 
 export default function RootLayout() {
   // Uncomment the following block before building the app to set the navigation bar color properly on Android
@@ -20,7 +67,12 @@ export default function RootLayout() {
         />
       )}
 
-      <Stack
+
+      <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} tokenCache={tokenCache}>
+      <InitialLayout />
+      </ClerkProvider>
+
+      {/* <Stack
         screenOptions={{
           contentStyle: { backgroundColor: "black" },
         }}
@@ -37,7 +89,7 @@ export default function RootLayout() {
             headerShown: false,
           }}
         />
-      </Stack>
+      </Stack> */}
       {/*Comment the following block before building the app to set the navigation bar color properly on Android*/}
   
       {Platform.OS === "android" && (
